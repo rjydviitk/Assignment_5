7bad1b4160ba82d7f84bc9fb2a8d8cc92bd230fe
Ramji Yadav <ramjiyadav22@iitk.ac.in>
2024-01-14 15:03:42 +0530

    New Changes

--- flutter_shopping_app/lib/main.dart
+++ flutter_shopping_app/lib/main.dart
@@ -1,125 +1,256 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Product {
+  final String image;
+  final String name;
+  final double price;
+
+  Product({required this.image, required this.name, required this.price});
+}
+
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem({required this.product, this.quantity = 1});
+}
+
+class Cart with ChangeNotifier {
+  List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+
+  void addToCart(Product product) {
+    for (var item in _items) {
+      if (item.product == product) {
+        item.quantity++;
+        notifyListeners();
+        return;
+      }
+    }
+    _items.add(CartItem(product: product));
+    notifyListeners();
+  }
+
+  void removeFromCart(Product product) {
+    _items.removeWhere((item) => item.product == product);
+    notifyListeners();
+  }
+
+  double getTotalPrice() {
+    double total = 0.0;
+    for (var item in _items) {
+      total += item.product.price * item.quantity;
+    }
+    return total;
+  }
+
+  void clearCart() {
+    _items.clear();
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => Cart(),
+      child: MaterialApp(
+        home: ProductListScreen(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ProductListScreen extends StatelessWidget {
+  final List<Product> products = [
+    Product(image: 'assets/images/1.jpg', name: 'Black T-shirt', price: 20.0),
+    Product(
+        image: 'assets/images/2.jpg', name: 'Off-White T-shirt', price: 25.0),
+    Product(image: 'assets/images/1.jpg', name: 'Product 1', price: 20.0),
+    Product(image: 'assets/images/2.jpg', name: 'Product 2', price: 25.0),
+    Product(image: 'assets/images/1.jpg', name: 'Product 1', price: 20.0),
+    Product(image: 'assets/images/2.jpg', name: 'Product 2', price: 25.0),
+    Product(image: 'assets/images/1.jpg', name: 'Product 1', price: 20.0),
+    Product(image: 'assets/images/2.jpg', name: 'Product 2', price: 25.0),
+    Product(image: 'assets/images/1.jpg', name: 'Product 1', price: 20.0),
+    Product(image: 'assets/images/2.jpg', name: 'Product 2', price: 25.0),
+    Product(image: 'assets/images/1.jpg', name: 'Product 1', price: 20.0),
+    Product(image: 'assets/images/2.jpg', name: 'Product 2', price: 25.0),
+    Product(image: 'assets/images/1.jpg', name: 'Product 1', price: 20.0),
+    Product(image: 'assets/images/2.jpg', name: 'Product 2', price: 25.0),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('T Shirts'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CartScreen(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ProductItem(product: products[index]);
+        },
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  ProductItem({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              product.image,
+              height: 650,
+              width: 569,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(product.name, style: TextStyle(fontSize: 18)),
+                  Text('\$${product.price.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 16)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: cart.items.any((item) => item.product == product)
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Added To Cart',
+                              style: TextStyle(
+                                fontSize: 14,
+                              )),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.remove),
+                          onPressed: () {
+                            cart.removeFromCart(product);
+                          },
+                        ),
+                      ],
+                    )
+                  : ElevatedButton(
+                      onPressed: () {
+                        cart.addToCart(product);
+                      },
+                      child:
+                          Text('Add to Cart', style: TextStyle(fontSize: 14)),
+                    ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    final cartItems = cart.items;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping Cart'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+                CartItem cartItem = cartItems[index];
+                return ListTile(
+                  leading: Image.asset(cartItem.product.image),
+                  title: Text(
+                    '${cartItem.product.name}\n\$${cartItem.product.price.toString()}',
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.remove),
+                        onPressed: () {
+                          cart.removeFromCart(cartItem.product);
+                        },
+                      ),
+                      Text('${cartItem.quantity}'),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          cart.addToCart(cartItem.product);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Summary',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                SizedBox(height: 8),
+                Text(
+                    'Cart Total: \$${cart.getTotalPrice().toStringAsFixed(2)}'),
+                SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    cart.clearCart();
+                  },
+                  child: Text('Buy Now'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
